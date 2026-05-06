23c99dcbbf513db0924253df0869860151737fb0
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-06 15:47:36 +0700

    Update login.php

--- brngerza/login.php
+++ brngerza/login.php
@@ -1,59 +1,70 @@
-<?php
-session_start();
-if (isset($_SESSION['username'])) {
-    header("Location: dashboard.php");
-    exit;
-}
-include 'koneksi.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['uname'];
-    $password = $_POST['psw'];
-    $query = "SELECT * FROM user WHERE username='$username'";
-    $result = mysqli_query($koneksi, $query);
-    $user = mysqli_fetch_assoc($result);
-    if ($user && $password === $user['password']) {
-        $_SESSION['username'] = $user['username'];
-        header("Location: dashboard.php");
-        exit;
-    } else {
-        $_SESSION['error'] = "Invalid username or password";
-        header("Location: login.php");
-        exit;
-    }
-}
-?>
-<!DOCTYPE html>
-<html>
 <head>
     <link rel="stylesheet" href="login.css">
+    <?php
+    session_start();
+
+    if (isset($_SESSION['username'])) {
+        header("Location: dashboard.php");
+        exit;
+    }
+    ?>
+
 </head>
-<body>
+
+
+
+<?php
+    include 'koneksi.php';
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $username = $_POST['uname'];
+        $password = $_POST['psw'];
+
+        $query = "SELECT * FROM user WHERE username='$username'";
+        $result = mysqli_query($koneksi, $query);
+        $user = mysqli_fetch_assoc($result);
+        if ($user && $password === $user['password']) {
+            $_SESSION['username'] = $user['username'];
+            
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            $_SESSION['error'] = "Invalid username or password";
+            header("Location: login.php");
+            exit;
+        }
+
+    }
+?>
+
 <?php
 if (isset($_SESSION['error'])) {
     echo "<script>alert('{$_SESSION['error']}');</script>";
     unset($_SESSION['error']);
 }
 ?>
+
+<body>
     <div class=base>
-        <div class="title">
-            <h2>Login Form</h2>
-        </div>
-        <div class="container">
+            <div class="title">
+                <h2>Login Form</h2>
+            </div>
+            <div class="container">
             <form method="post">
                 <div class="uname" style="padding:7px">
-                    <label for="uname"><b>Username </b></label><br>
+                    <label for="uname"><b>Username </b></label></br>
                     <input type="text" placeholder="Enter Username" name="uname" required>
                 </div>
-                <br>
+                </br>
                 <div class="psw" style="padding:7px">
-                    <label for="psw"><b>Password  </b></label><br>
+                    <label for="psw"><b>Password  </b></label></br>
                     <input type="password" placeholder="Enter Password" name="psw" required>
-                </div>
-                <br>
-                <button type="submit" name="login">Login</button>
+                </br>
+</div>
+</br>
+                <button type="submit" name ="login">Login</button>
             </form>
         </div>
     </div>
 </body>
-</html>
