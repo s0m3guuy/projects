ea9699d61f9a7a1a64c12fa596fd718100139bad
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-06 15:35:02 +0700

    Update koneksi.php

--- brngerza/koneksi.php
+++ brngerza/koneksi.php
@@ -1,11 +1,11 @@
 <?php
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$db   = getenv('MYSQLDATABASE'); // this will return 'railway'
-$port = getenv('MYSQLPORT');
+$host = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$user = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$pass = $_ENV['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
+$db   = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$port = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
 
-$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
+$koneksi = mysqli_connect($host, $user, $pass, $db, (int)$port);
 
 if (!$koneksi) {
     die('Connection failed: ' . mysqli_connect_error());
