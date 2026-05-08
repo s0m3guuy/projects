af1fcab14f0f2f75669d303e4ef00034c4ef746f
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-08 14:40:48 +0700

    Update dashboard.php

--- brngerza/dashboard.php
+++ brngerza/dashboard.php
@@ -387,7 +387,7 @@ $totalAmount = $totalAmountRow['total'] ?? 0;
 <div class="layout">
     <!-- SIDEBAR -->
     <aside class="sidebar" id="sidebar">
-        <img src="gmbr/logo.jpeg" class="logo">
+        <img src="gmbr/logo.png" class="logo">
             <nav>
                 <a href="dashboard.php" class="active"><i class="bi bi-speedometer2"></i><span>Dashboard</span></a>
                 <a href="anggota.php"><i class="bi bi-people"></i><span>Anggota</span></a>
