eb43b217b37ae4547dbd049722b9dbc543c1a54a
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-15 20:34:36 +0700

    Delete mobileerza/brngerza directory

--- mobileerza/brngerza/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi=mysqli_connect("localhost","root","mysql","ahmad");
-echo("");
-
-if (mysqli_connect_error()){
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}   
-?>
