967738f502c23079863de7ecfba30f73cab19e8a
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-19 11:18:13 +0700

    Update temppass.php

--- temppass.php
+++ temppass.php
@@ -1,3 +1,16 @@
 <?php
-echo password_hash('your_actual_password', PASSWORD_DEFAULT);
+if(isset($_POST['password'])) {
+    $hashed = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    echo "<p>Hash: <strong>" . $hashed . "</strong></p>";
+    echo "<p>Copy the hash above and paste it into your database.</p>";
+}
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+    <form method="POST">
+        <input type="text" name="password" placeholder="Enter password to hash">
+        <button type="submit">Generate Hash</button>
+    </form>
+</body>
+</html>
