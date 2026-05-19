478d979a6d077bfcdd47c9b32e1a630e41390dd2
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-19 13:02:47 +0700

    Update laporan2.php

--- mobileerza/laporan2.php
+++ mobileerza/laporan2.php
@@ -226,7 +226,7 @@ setTimeout(function(){$('.alert').alert('close');},5000);
         <i class="bi bi-speedometer2"></i>
         <span>Dashboard</span>
     </a>
-    <a href="anggota.php" class="active">
+    <a href="anggota.php" >
         <i class="bi bi-people"></i>
         <span>Anggota</span>
     </a>
@@ -265,7 +265,7 @@ setTimeout(function(){$('.alert').alert('close');},5000);
       <span>Bayar</span>
     </a>
   
-    <a href="laporan2.php">
+    <a href="laporan2.php" class="active">
       <i class="bi bi-file-earmark-bar-graph"></i>
       <span>Keuangan</span>
     </a>
