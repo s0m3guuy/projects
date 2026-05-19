52b3888f4b7167b9881b4febb1478fbdd9db31e6
s0m3guuy <fieryfoxgaming@gmail.com>
2026-05-19 11:57:29 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require 'koneksi.php';
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $query = "SELECT * FROM users WHERE username = ?";
+    $query = "SELECT * FROM user WHERE username = ?";
     $stmt = mysqli_prepare($koneksi, $query);
     mysqli_stmt_bind_param($stmt, 's', $username);
     mysqli_stmt_execute($stmt);
